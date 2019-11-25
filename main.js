82cbc2ce6c21f0c827ae65b312f9b9e4ef8b4eac
felice7 <feliga76@gmail.com>
2019-11-25 19:28:34 +0100

    inserimento istruzioni in javascript per l'icona hamburger

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.header-right > a').click(function(){
+        $('.hamburger-menu').addClass('active')
+        
+    })    
+        
+    $('.hamburger-menu > a').click(function(){
+        $('.hamburger-menu').removeClass('active')
+        
+    })    
+
+});
